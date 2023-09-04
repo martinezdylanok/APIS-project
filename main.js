a7f3f08223ae611720e8290e68168544aed26337
Lpik97 <dm97fx@gmail.com>
2023-09-04 11:23:51 +0200

    feat(img element selected, fetch function)
    The img element was selected from the body. The fetch function will use the api from GIPHY with the cors mode making a promise that positive will make the response be a config as json, and then used as src for the img.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-var HELLO = "World !";
+const img = document.querySelector("img");
+fetch("https://api.giphy.com/v1/gifs/translate?api_key=wKXpOjRC2bbTrBrQNVCiGI7fQOxsRhzI&s=cats", { mode: "cors" })
+   .then(function (response) {
+      return response.json();
+   })
+   .then(function (response) {
+      img.src = response.data.images.original.url;
+   });
