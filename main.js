37fc0bf4841be2fee0d2286c8bcdeacaf113d4ef
Lpik97 <dm97fx@gmail.com>
2023-09-05 11:28:11 +0200

    feat(call the gifs function imported and invoked, small fix in dog button id)

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import callTheCats from "./js_modules/callTheCats.js";
 import callTheDogs from "./js_modules/callTheDogs.js";
+import callTheGifs from "./js_modules/callTheGifs.js";
+
+const FORM = document.querySelector("form");
+FORM.addEventListener("submit", callTheGifs);
 
 callTheCats();
 
-const BUTTON = document.querySelector("button");
+const BUTTON = document.getElementById("dog-button");
 BUTTON.addEventListener("click", callTheDogs);
