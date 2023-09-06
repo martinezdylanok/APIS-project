924fe03e9744f4bb59ec84e4bcf07b862fc52f50
Lpik97 <dm97fx@gmail.com>
2023-09-06 11:10:00 +0200

    refactor(refactor of the callTheCats function)

--- js_modules/callTheCats.js
+++ js_modules/callTheCats.js
@@ -1,12 +1,8 @@
-function callTheCats() {
+async function callTheCats() {
    const CAT_GIF = document.getElementById("cat-gif");
-   fetch("https://api.giphy.com/v1/gifs/translate?api_key=wKXpOjRC2bbTrBrQNVCiGI7fQOxsRhzI&s=cats", { mode: "cors" })
-      .then(function (response) {
-         return response.json();
-      })
-      .then(function (response) {
-         CAT_GIF.src = response.data.images.original.url;
-      });
+   const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=wKXpOjRC2bbTrBrQNVCiGI7fQOxsRhzI&s=cats", { mode: "cors" });
+   const catData = await response.json();
+   CAT_GIF.src = catData.data.images.original.url;
 }
 
 export default callTheCats;
